66b595f300136ae773a709a89579c4a18f47b2a9
Franz Kiermaier <franz.kiermaier@software-creative.de>
2018-03-26 11:56:53 +0200

    Projektdateien hinzufügen.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=noerrbasicbot-Web-Deploy.pubxml -p:Password=J2kqrjhzoirpap1mDna6sKwG8ux2ow1wJfAtaG71JprDnwtX4MlvoRX687k7
+
